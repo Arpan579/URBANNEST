d83be29a6a7df8d8b79b59bcea2bac0dffb2786e
Arpan Pramanik <pramanikarpan800@gmail.com>
2026-08-21 17:55:39 +0530

    Revert to separate deployments

--- server/app.js
+++ server/app.js
@@ -25,25 +25,10 @@ app.use('/api/products', productRoutes);
 app.use('/api/queries', queryRoutes);
 app.use('/api/chatbot', chatbotRoutes);
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-if (config.NODE_ENV === 'production') {
-  // Serve static files from the React frontend build
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'))
-  );
-} else {
-  // Root route for development
-  app.get('/', (req, res) => {
-    res.send('UrbanNest API is running...');
-  });
-}
+// Root route
+app.get('/', (req, res) => {
+  res.send('UrbanNest API is running...');
+});
 
 // Error Handling Middleware
 app.use(notFoundMiddleware);
